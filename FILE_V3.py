ea218d764bbe864d35485ee7ec14c1f3578dda4e
Badshawazir <105278941+Badshawazir@users.noreply.github.com>
2023-03-03 15:01:03 +0300

    Update FILE_V3.py

--- FILE_V3.py
+++ FILE_V3.py
@@ -267,7 +267,7 @@ dic = {'1':'January','2':'February','3':'March','4':'April','5':'May','6':'June'
 dic2 = {'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'Devember'}
 
 
-thn = datetime.datetime.now().year
+
 okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
 cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
 #------------------[ MACHINE-SUPPORT ]---------------#
